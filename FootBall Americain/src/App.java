6a88cd6e36026904bf2a0b68bb8a2be4f2e384d8
Alec <salamandeurs@gmail.com>
2018-09-06 00:10:48 -0400

    VueAjouterJoueur

--- FootBall Americain/src/App.java
+++ FootBall Americain/src/App.java
@@ -3,7 +3,8 @@ public class App {
 	public static void main(String[] parametres) {
 
 		//VueJoueur.launch(VueJoueur.class, parametres);
-		VueListeJoueur.launch(VueListeJoueur.class, parametres);
+		//VueListeJoueur.launch(VueListeJoueur.class, parametres);
+		VueAjouterJoueur.launch(VueAjouterJoueur.class, parametres);
 	}
 
 }
---
+++ FootBall Americain/src/VueAjouterJoueur.java
@@ -0,0 +1,53 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class VueAjouterJoueur extends Application {
+
+
+	protected TextField valeurNom;
+	protected TextField valeurNumero;
+	protected TextField valeurPoids;
+	protected TextField valeurNaissance;
+	
+	@Override
+	public void start(Stage stade) throws Exception {
+
+		VBox panneau = new VBox();	
+		GridPane grilleJoueur = new GridPane();
+		
+		valeurNom = new TextField();
+		grilleJoueur.add(new Label("Nom : "), 0, 0);
+		grilleJoueur.add(valeurNom, 1, 0);
+		
+		valeurNumero = new TextField("");
+		grilleJoueur.add(new Label("Numero : "), 0, 1);
+		grilleJoueur.add(valeurNumero, 1, 1);
+	
+		valeurPoids = new TextField("");
+		grilleJoueur.add(new Label("Poids : "), 0, 2);
+		grilleJoueur.add(valeurPoids, 1, 2);	
+		
+		valeurNaissance = new TextField("");
+		grilleJoueur.add(new Label("Naissance : "), 0, 3);
+		grilleJoueur.add(valeurNaissance, 1, 3);
+		
+		panneau.getChildren().add(new Label("Ajouter un joueur"));
+		panneau.getChildren().add(grilleJoueur);
+		panneau.getChildren().add(new Button("Enregistrer"));
+		stade.setScene(new Scene(panneau, 400, 400));
+		stade.show();		
+	
+	}
+	
+	public Joueur demanderJoueur()
+	{
+		return null;
+	}
+
+}
