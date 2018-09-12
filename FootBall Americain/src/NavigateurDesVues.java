d0a4933c0025c1e01fa8699d24b28f100295d9a9
Alec <salamandeurs@gmail.com>
2018-09-12 13:24:20 -0400

    Déplacement des test dans le navigateur
    VueListeJoueur -> NavigateurDesVues
    VueJoueur -> NavigateurDesVues

--- FootBall Americain/src/NavigateurDesVues.java
+++ FootBall Americain/src/NavigateurDesVues.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -12,12 +15,24 @@ public class NavigateurDesVues extends Application {
 		this.vueAjouterJoueur = new VueAjouterJoueur();
 		this.vueListeJoueur = new VueListeJoueur();
 		this.vueJoueur = new VueJoueur();
+		
+
+		List<Joueur> listeJoueurTest = new ArrayList<Joueur>();
+		
+		listeJoueurTest.add(new Joueur("Alec", "	22", "	59 kg", "	4 Mai 1998"));
+		listeJoueurTest.add(new Joueur("Lucas", "	12", "	76 kg", "	10 Juillet 1997"));
+		listeJoueurTest.add(new Joueur("Antoine", "	2", "	59 kg", "	20 Octobre 1998"));
+		listeJoueurTest.add(new Joueur("Romain", "	77", "	80 kg", "	5 septembre 1997"));
+ 		this.vueListeJoueur.afficherListeJoueur(listeJoueurTest);
+ 		
+ 		Joueur joueur = new Joueur("	Alec", "	22", "	59 kg", "	4 Mai 1998");
+ 		this.vueJoueur.afficherMouton(joueur);
 	}
 	@Override
 	public void start(Stage stade) throws Exception {
 	
-		stade.setScene(this.vueAjouterJoueur);
-		stade.setScene(this.vueListeJoueur);
+	//	stade.setScene(this.vueAjouterJoueur);
+		//stade.setScene(this.vueListeJoueur);
 		stade.setScene(this.vueJoueur);
 		stade.show();
 	}
--- FootBall Americain/src/VueListeJoueur.java
+++ FootBall Americain/src/VueListeJoueur.java
@@ -19,14 +19,7 @@ public class VueListeJoueur extends Scene{
 		
 		panneau.getChildren().add(grilleJoueur);
 		
-		
-		List<Joueur> listeJoueurTest = new ArrayList<Joueur>();
-		
-		listeJoueurTest.add(new Joueur("Alec", "	22", "	59 kg", "	4 Mai 1998"));
-		listeJoueurTest.add(new Joueur("Lucas", "	12", "	76 kg", "	10 Juillet 1997"));
-		listeJoueurTest.add(new Joueur("Antoine", "	2", "	59 kg", "	20 Octobre 1998"));
-		listeJoueurTest.add(new Joueur("Romain", "	77", "	80 kg", "	5 septembre 1997"));
- 		this.afficherListeJoueur(listeJoueurTest); 
+		 
  		
  		
 		
