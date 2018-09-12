12084c9e31eff80e69c5521587cb5b23fc05fc00
Alec <salamandeurs@gmail.com>
2018-09-12 17:37:34 -0400

    Creation de la class JoueurDAO

--- FootBall Americain/src/Controleur/ControleurJoueur.java
+++ FootBall Americain/src/Controleur/ControleurJoueur.java
@@ -3,6 +3,7 @@ package Controleur;
 import java.util.ArrayList;
 import java.util.List;
 
+import Donnee.JoueurDAO;
 import Modele.Joueur;
 import Vue.NavigateurDesVues;
 import Vue.VueAjouterJoueur;
@@ -38,17 +39,14 @@ public class ControleurJoueur {
 		
 		this.navigateur.naviguerVerVueJoueur();
 		this.navigateur.naviguerVersVueListeJoueur();
-		this.navigateur.naviguerVersVueAjouterJoueur();
+		//this.navigateur.naviguerVersVueAjouterJoueur();
 	
-		/*Test*/
-List<Joueur> listeJoueurTest = new ArrayList<Joueur>();
-		
-		listeJoueurTest.add(new Joueur("Alec", "	22", "	59 kg", "	4 Mai 1998"));
-		listeJoueurTest.add(new Joueur("Lucas", "	12", "	76 kg", "	10 Juillet 1997"));
-		listeJoueurTest.add(new Joueur("Antoine", "	2", "	59 kg", "	20 Octobre 1998"));
-		listeJoueurTest.add(new Joueur("Romain", "	77", "	80 kg", "	5 septembre 1997"));
+
+		JoueurDAO joueurDAO = new JoueurDAO();
+		List<Joueur> listeJoueurTest = joueurDAO.listerJoueur();
  		this.vueListeJoueur.afficherListeJoueur(listeJoueurTest);
 	
+ 			
  		
 	}
 	
